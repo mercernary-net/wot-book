5f4bced0bb599fad2f5dd06f8b8a3e08dbf12424
mercernary-net <122980733+mercernary-net@users.noreply.github.com>
2023-01-21 13:31:56 +0000

    new blink.js with initial alert about how to stop programme

--- chapter4-gpios/blink.js
+++ chapter4-gpios/blink.js
@@ -1,28 +1,32 @@
-var onoff = require('onoff'); //#A
+// let user know how to stop programme
+console.log( "Press CTRL C to stop programme" );
 
-var Gpio = onoff.Gpio,
-  led = new Gpio(4, 'out'), //#B
-  interval;
+// import the onoff library
+var onoff = require onoff;
 
-interval = setInterval(function () { //#C
-  var value = (led.readSync() + 1) % 2; //#D
-  led.write(value, function() { //#E
-    console.log("Changed LED state to: " + value);
+// initialise GPIO pin 4 to be an output pin
+var Gpio = onoff.Gpio, 
+                    led = new Gpio( 4, 'out' ),
+                    interval;
+
+interval = setInterval( function () {
+  // synchronously read the value of pin 4 and transform it
+ // if 1 change it to 0,if 0 change it to 1
+  var value = (led.readSync() + 1 ) % 2;
+
+  // Asynchronously write the new value to pin 4
+  led.write( value, function () {
+    console.log( "Changed LED state to " + value );
   });
 }, 2000);
 
-process.on('SIGINT', function () { //#F
-  clearInterval(interval);
-  led.writeSync(0); //#G
+// listen to the event triggered by pressing CTRL+C
+process.on( 'SIGINT' function() {
+  clearInterval( interval);
+  led.writeSync(0);
+
+  // cleanly close the GPIO pin before exiting
   led.unexport();
-  console.log('Bye, bye!');
   process.exit();
 });
 
-// #A Import the onoff library
-// #B Initialize pin 4 to be an output pin
-// #C This interval will be called every 2 seconds
-// #D Synchronously read the value of pin 4 and transform 1 to 0 or 0 to 1
-// #E Asynchronously write the new value to pin 4
-// #F Listen to the event triggered on CTRL+C
-// #G Cleanly close the GPIO pin before exiting
