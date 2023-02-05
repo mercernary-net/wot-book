79d2c6ec614fa4c76450a9b641579a5e6e3cd62d
mercernary-net <122980733+mercernary-net@users.noreply.github.com>
2023-02-05 18:12:35 +0000

    Add files via upload
    added edukit 3-temperature.py for the thermometer

---
+++ chapter4-gpios/3-temperature.py
@@ -0,0 +1,13 @@
+
+import time
+from w1thermsensor import W1ThermSensor, Unit
+sensor = W1ThermSensor()
+
+while True:
+ temp_C = sensor.get_temperature(Unit.DEGREES_C)
+ temp_F = sensor.get_temperature(Unit.DEGREES_F)
+
+print(temp_C, " Celsius")
+print(temp_F, " Fahrenheit")
+
+time.sleep(2)
