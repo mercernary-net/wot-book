14c810a6f06088b96aa4700f4cbc07a6d7dc6a23
mercernary-net <122980733+mercernary-net@users.noreply.github.com>
2023-02-07 17:17:11 +0000

    Add files via upload
    new dht.js file complete with process.on() function

--- chapter4-gpios/dht.js
+++ chapter4-gpios/dht.js
@@ -1,23 +1,20 @@
-var sensorLib = require('node-dht-sensor');
+console.log( 'dht.js running' );
+console.log( 'press CTRL+C to stop!' );
 
-sensorLib.initialize(22, 12); //#A
-var interval = setInterval(function () { //#B
+var sensorLib = require( 'node-dht-sensor');
+sensorLib.initialize( 22, 12 );
+var interval = setInterval( function () {
   read();
-}, 2000);
+}, 2000 );
 
 function read() {
-  var readout = sensorLib.read(); //#C
-  console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' + //#D
-    'humidity: ' + readout.humidity.toFixed(2) + '%');
-};
+  var readout = sensorLib.read();
+  console.log( 'Temperature: ' + readout.temperature.toFixed(2) + 'C, ' + 'humidity: ' + readout.humidity.toFixed(2) + '%' );
+}
 
-process.on('SIGINT', function () {
+
+process.on( 'SIGINT', function () {
   clearInterval(interval);
   console.log('Bye, bye!');
   process.exit();
 });
-
-//#A 22 is for DHT22/AM2302, 12 is the GPIO we connect to on the Pi
-//#B create an interval to read the values every 2 seconds
-//#C read the sensor values
-//#D readout contains two values: temperature and humidity
