f30872ec155692283d225a80990dffa3e90c6d26
mercernary-net <122980733+mercernary-net@users.noreply.github.com>
2023-02-09 08:40:34 +0000

    Add files via upload
    increased interval Period to 5 mins

--- chapter4-gpios/dht.js
+++ chapter4-gpios/dht.js
@@ -1,20 +1,25 @@
 console.log( 'dht.js running' );
 console.log( 'press CTRL+C to stop!' );
 
+var intervalPeriod = 300000;
+const MIN_TEMP = 18.0;
+const MAX_TEMP = 20.0;
 var sensorLib = require( 'node-dht-sensor');
+
 sensorLib.initialize( 22, 12 );
+
 var interval = setInterval( function () {
   read();
-}, 60000 );
+}, intervalPeriod );
 
 function read() {
   var readout = sensorLib.read();
   var d = new Date().toISOString().slice(0, 19).replace('T', ' ');
   var sensorId          = readout.id;
-  
+  var min = MIN_TEMP;
+  var max = MAX_TEMP;
   var temperature = readout.temperature.toFixed(2);
-  var min = 19.0;
-  var max = 24.0;
+  
   console.log( d + 'console: ' + sensorId + ' reports Temperature: ' + temperature + 'C, ' + ', and humidity: ' + readout.humidity.toFixed(2) + '%' );
   if ( min > temperature ) {
     console.log( "Brrr! it's too cold: turn on the heating!" );
@@ -27,7 +32,7 @@ function read() {
 
 
 process.on( 'SIGINT', function () {
-  clearInterval(interval);
+  clearInterval( interval );
   console.log('Bye, bye!');
   process.exit();
 });
