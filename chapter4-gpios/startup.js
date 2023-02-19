44b431af0f87d6f95607e7aee2de1a4c9bfbbfd8
mercernary-net <122980733+mercernary-net@users.noreply.github.com>
2023-02-19 15:04:20 +0000

    temporarily added startup.js to wot-book directory

---
+++ chapter4-gpios/startup.js
@@ -0,0 +1,6 @@
+console.log( 'Welcome to Raspberry Pi' );
+console.log( 'Available Apps: ');
+console.log( 'dht.js - receive reports of ambient temperature: ' );
+console.log( 'type: cd wot-book/chapter4-gpios' );
+console.log( 'type: node dht.js' );
+console.log( '-----------------------');
